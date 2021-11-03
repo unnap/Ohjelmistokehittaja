b9b40ae38055c57dddbb1ef7c39d12ec33252216
unnap <59951708+unnap@users.noreply.github.com>
2021-11-03 11:31:29 +0200

    Add files via upload

---
+++ Harjoitukset4/Harjoitukset4/Navig.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Harjoitukset4
+{
+    class Navig
+    {
+        public static void Valikko()
+        {
+        alku:
+            Console.WriteLine("1) Harjoitus 1\n2) Harjoitus 2\n3) Harjoitus 3\n4) Harjoitus 4\n5) Harjoitus 5\n6) Harjoitus 6\n7) Harjoitus 7\n8) Lopetus");
+            Console.WriteLine("Valitse harjoitus kirjoittamalla numero");
+            int valinta = Convert.ToInt32(Console.ReadLine());
+            switch (valinta)
+            {
+                case 1:
+                    Program.Harjoitus1();
+                    break;
+                 case 2:
+                     Program.Harjoitus2();
+                     break;
+                 case 3:
+                     Program.Harjoitus3();
+                     break;
+                 case 4:
+                     Program.Harjoitus4();
+                     break;
+                 case 5:
+                     Program.Harjoitus5();
+                     break;
+                /* case 6:
+                     Harjoitus6();
+                     break;
+                 case 7:
+                     Harjoitus7();
+                     break;*/
+                case 8:
+                    Console.WriteLine("Heippa");
+                    break;
+                default:
+                    Console.WriteLine("VIRHE\nKirjoita numero oikeassa muodossa");
+                    goto alku;
+            }
+        }
+
+        public static void Paluu()
+        {
+        valinta:
+            Console.Write("Haluatko palata alkuun? (k/e) ");
+            char paluu = Convert.ToChar(Console.ReadLine());
+            switch (paluu)
+            {
+                case 'k':
+                    Valikko();
+                    break;
+                case 'e':
+                    Console.WriteLine("Heippa");
+                    break;
+                default:
+                    Console.WriteLine("VIRHE\nKirjoita vain k tai e");
+                    goto valinta;
+            }
+        }
+    }
+}
---
+++ Harjoitukset4/Harjoitukset4/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+
+//UPS mä nyt tein nää tehtävät suoraan niin että käyttäjä syöttää tiedot
+//... musta tuntuu et oon tehny nää tehtävät jo aiemmin mut en jaksa tarkistaa
+//mut tuli ainakin harjoittelua classien käytöstä
+
+namespace Harjoitukset4
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Navig.Valikko();
+        }
+
+
+
+        public static void Harjoitus1()
+        {
+            double lukuX;
+            double lukuY;
+            Console.WriteLine("HARJOITUS 1");
+            Console.WriteLine("----------");
+            paluu1:
+            Console.Write("Anna numero ");
+            bool syotto1 = double.TryParse(Console.ReadLine(), out lukuX);
+            if (syotto1)
+            {
+                paluu2:
+                Console.Write("Anna toinen numero ");
+                bool syotto2 = double.TryParse(Console.ReadLine(), out lukuY);
+                if (syotto2)
+                {
+                    Console.WriteLine(lukuX + " + " + lukuY + " = " + (lukuX + lukuY));
+                    Navig.Paluu();
+                } else
+                {
+                    Console.WriteLine("VIRHE\nKäytä vain numeroita");
+                    goto paluu2;
+                }
+            } else
+            {
+                Console.WriteLine("VIRHE\nKäytä vain numeroita");
+                goto paluu1;
+            }
+        }
+
+        public static void Harjoitus2()
+        {
+            double lukuX;
+            Console.WriteLine("HARJOITUS 2");
+            Console.WriteLine("----------");
+            Console.WriteLine("Celsius muunnos Fahrenheiteiksi");
+        paluu1:
+            Console.Write("Anna Celsius-asteet ");
+            bool syotto1 = double.TryParse(Console.ReadLine(), out lukuX);
+            if (syotto1)
+            {
+                Console.WriteLine(lukuX + "C = " + ((lukuX * 1.8) + 32) + "F");
+                Navig.Paluu();
+            }
+            else
+            {
+                Console.WriteLine("VIRHE\nAnna asteet numeroina");
+                goto paluu1;
+            }
+        }
+
+        public static void Harjoitus3()
+        {
+            double lukuX;
+            double lukuY;
+            Console.WriteLine("HARJOITUS 3");
+            Console.WriteLine("----------");
+        paluu1:
+            Console.Write("Anna numero ");
+            bool syotto1 = double.TryParse(Console.ReadLine(), out lukuX);
+            if (syotto1)
+            {
+            paluu2:
+                Console.Write("Anna toinen numero ");
+                bool syotto2 = double.TryParse(Console.ReadLine(), out lukuY);
+                if (syotto2)
+                {
+                    Console.WriteLine(lukuX + " + " + lukuY + " = " + (lukuX + lukuY));
+                    Console.WriteLine(lukuX + " - " + lukuY + " = " + (lukuX - lukuY));
+                    Console.WriteLine(lukuX + " * " + lukuY + " = " + (lukuX * lukuY));
+                    Console.WriteLine(lukuX + " / " + lukuY + " = " + (lukuX / lukuY));
+                    Navig.Paluu();
+                }
+                else
+                {
+                    Console.WriteLine("VIRHE\nKäytä vain numeroita");
+                    goto paluu2;
+                }
+            }
+            else
+            {
+                Console.WriteLine("VIRHE\nKäytä vain numeroita");
+                goto paluu1;
+            }
+        }
+
+        public static void Harjoitus4()
+        {
+            double lukuX;
+            double lukuY;
+            Console.WriteLine("HARJOITUS 4");
+            Console.WriteLine("----------");
+        paluu1:
+            Console.Write("Anna numero ");
+            bool syotto1 = double.TryParse(Console.ReadLine(), out lukuX);
+            if (syotto1)
+            {
+            paluu2:
+                Console.Write("Anna toinen numero ");
+                bool syotto2 = double.TryParse(Console.ReadLine(), out lukuY);
+                if (syotto2)
+                {
+                    Console.WriteLine(lukuX + " % " + lukuY + " = " + (lukuX % lukuY));
+                    Navig.Paluu();
+                }
+                else
+                {
+                    Console.WriteLine("VIRHE\nKäytä vain numeroita");
+                    goto paluu2;
+                }
+            }
+            else
+            {
+                Console.WriteLine("VIRHE\nKäytä vain numeroita");
+                goto paluu1;
+            }
+        }
+
+        public static void Harjoitus5()
+        {
+            double lukuX;
+            Console.WriteLine("HARJOITUS 5");
+            Console.WriteLine("----------");
+        paluu1:
+            Console.Write("Anna nimi ");
+            string nimi = Console.ReadLine();
+            bool syotto1 = double.TryParse(nimi, out lukuX);
+            if (!syotto1)
+            {
+                Console.Write("Hei " + nimi);
+            }
+            else
+            {
+                Console.WriteLine("VIRHE\nEn hyväksy numeroa nimeksi :(");
+                goto paluu1;
+            }
+        }
+    }
+}
