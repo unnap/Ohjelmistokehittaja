b0df4006d4332f1bc6c783e4f2f3a20459207aea
unnap <59951708+unnap@users.noreply.github.com>
2021-11-15 11:27:36 +0200

    Add files via upload

---
+++ Laskin/Laskin/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Laskin
+{
+    partial class Laskin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LukuYksiTB = new System.Windows.Forms.TextBox();
+            this.LukuKaksiTB = new System.Windows.Forms.TextBox();
+            this.LaskutoimitusCB = new System.Windows.Forms.ComboBox();
+            this.YhtaKuinLB = new System.Windows.Forms.Label();
+            this.VastausLB = new System.Windows.Forms.Label();
+            this.LaskeBT = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LukuYksiTB
+            // 
+            this.LukuYksiTB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.LukuYksiTB.Font = new System.Drawing.Font("Tandysoft", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LukuYksiTB.ForeColor = System.Drawing.Color.Black;
+            this.LukuYksiTB.Location = new System.Drawing.Point(47, 41);
+            this.LukuYksiTB.Name = "LukuYksiTB";
+            this.LukuYksiTB.Size = new System.Drawing.Size(100, 34);
+            this.LukuYksiTB.TabIndex = 0;
+            // 
+            // LukuKaksiTB
+            // 
+            this.LukuKaksiTB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.LukuKaksiTB.Font = new System.Drawing.Font("Tandysoft", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LukuKaksiTB.ForeColor = System.Drawing.Color.Black;
+            this.LukuKaksiTB.Location = new System.Drawing.Point(198, 41);
+            this.LukuKaksiTB.Name = "LukuKaksiTB";
+            this.LukuKaksiTB.Size = new System.Drawing.Size(100, 34);
+            this.LukuKaksiTB.TabIndex = 1;
+            // 
+            // LaskutoimitusCB
+            // 
+            this.LaskutoimitusCB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.LaskutoimitusCB.Font = new System.Drawing.Font("Tandysoft", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LaskutoimitusCB.ForeColor = System.Drawing.Color.Black;
+            this.LaskutoimitusCB.FormattingEnabled = true;
+            this.LaskutoimitusCB.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.LaskutoimitusCB.Location = new System.Drawing.Point(153, 41);
+            this.LaskutoimitusCB.Name = "LaskutoimitusCB";
+            this.LaskutoimitusCB.Size = new System.Drawing.Size(39, 35);
+            this.LaskutoimitusCB.TabIndex = 2;
+            // 
+            // YhtaKuinLB
+            // 
+            this.YhtaKuinLB.AutoSize = true;
+            this.YhtaKuinLB.Font = new System.Drawing.Font("Tandysoft", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.YhtaKuinLB.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.YhtaKuinLB.Location = new System.Drawing.Point(304, 44);
+            this.YhtaKuinLB.Name = "YhtaKuinLB";
+            this.YhtaKuinLB.Size = new System.Drawing.Size(24, 27);
+            this.YhtaKuinLB.TabIndex = 3;
+            this.YhtaKuinLB.Text = "=";
+            // 
+            // VastausLB
+            // 
+            this.VastausLB.AutoSize = true;
+            this.VastausLB.Font = new System.Drawing.Font("Tandysoft", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VastausLB.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.VastausLB.Location = new System.Drawing.Point(334, 44);
+            this.VastausLB.Name = "VastausLB";
+            this.VastausLB.Size = new System.Drawing.Size(24, 27);
+            this.VastausLB.TabIndex = 4;
+            this.VastausLB.Text = "X";
+            this.VastausLB.Visible = false;
+            // 
+            // LaskeBT
+            // 
+            this.LaskeBT.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.LaskeBT.FlatAppearance.BorderColor = System.Drawing.Color.Lime;
+            this.LaskeBT.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.LaskeBT.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.LaskeBT.Font = new System.Drawing.Font("Tandysoft", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LaskeBT.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.LaskeBT.Location = new System.Drawing.Point(116, 103);
+            this.LaskeBT.Name = "LaskeBT";
+            this.LaskeBT.Size = new System.Drawing.Size(112, 47);
+            this.LaskeBT.TabIndex = 5;
+            this.LaskeBT.Text = "Laske";
+            this.LaskeBT.UseVisualStyleBackColor = true;
+            this.LaskeBT.Click += new System.EventHandler(this.LaskeBT_Click);
+            // 
+            // Laskin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(612, 185);
+            this.Controls.Add(this.LaskeBT);
+            this.Controls.Add(this.VastausLB);
+            this.Controls.Add(this.YhtaKuinLB);
+            this.Controls.Add(this.LaskutoimitusCB);
+            this.Controls.Add(this.LukuKaksiTB);
+            this.Controls.Add(this.LukuYksiTB);
+            this.Name = "Laskin";
+            this.Text = "Yksinkertainen nelilaskin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox LukuYksiTB;
+        private System.Windows.Forms.TextBox LukuKaksiTB;
+        private System.Windows.Forms.ComboBox LaskutoimitusCB;
+        private System.Windows.Forms.Label YhtaKuinLB;
+        private System.Windows.Forms.Label VastausLB;
+        private System.Windows.Forms.Button LaskeBT;
+    }
+}
+
---
+++ Laskin/Laskin/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laskin
+{
+    public partial class Laskin : Form
+    {
+        public Laskin()
+        {
+            InitializeComponent();
+        }
+
+        private void LaskeBT_Click(object sender, EventArgs e)
+        {
+            float lasku = 0;
+            float num1 = float.Parse(LukuYksiTB.Text);
+            float num2 = float.Parse(LukuKaksiTB.Text);
+            switch(LaskutoimitusCB.Text)
+            {
+                case "+":
+                    lasku = num1 + num2;
+                    break;
+                case "-":
+                    lasku = num1 - num2;
+                    break;
+                case "*":
+                    lasku = num1 * num2;
+                    break;
+                case "/":
+                    if (num2 == 0)
+                    {
+                        VastausLB.Text = "Nollalla ei voi jakaa";
+                        goto HYPPY;
+                    } else
+                    {
+                        lasku = num1 / num2;
+                    }
+                    break;
+            }
+            VastausLB.Text = lasku.ToString();
+        HYPPY:
+            VastausLB.Visible = true;
+        }
+    }
+}
